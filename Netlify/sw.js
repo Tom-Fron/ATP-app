d24e6ec4a78852080dcd2be586a5f9f74e711009
Toshi <toms77747@gmail.com>
2026-02-03 19:43:39 +0700

    問題追加test

---
+++ Netlify/sw.js
@@ -0,0 +1,51 @@
+const CACHE_NAME = 'japan-life-cache';
+const urlsToCache = [
+  './',
+  './index.html',
+  './manifest.json',
+  './icon.png',
+  './icon-512.png',
+];
+
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+  return self.clients.claim();
+});
+
+self.addEventListener('fetch', (event) => {
+  const url = event.request.url;
+  if (!url.startsWith('http://') && !url.startsWith('https://')) return;
+
+if (event.request.method !== 'GET') return;
+
+  event.respondWith(
+    fetch(event.request).then((networkResponse) => {
+      if (networkResponse.status === 206) {
+        return networkResponse;
+      }
+      return caches.open(CACHE_NAME).then((cache) => {
+        cache.put(event.request, networkResponse.clone());
+        return networkResponse;
+      });
+    }).catch(() => {
+      return caches.match(event.request);
+    })
+  );
+});
