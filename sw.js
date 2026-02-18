14f0ccb7a61859d73d123e710bbff3a47c9ccf10
Toshi <toms77747@gmail.com>
2026-02-18 12:37:28 +0700

    rev sw.js

--- sw.js
+++ sw.js
@@ -32,30 +32,21 @@ self.addEventListener('activate', (event) => {
 self.addEventListener('fetch', (event) => {
   const request = event.request;
 
-  // GET以外は無視
   if (request.method !== 'GET') return;
 
-  // 自分のサイト以外は無視（超重要）
   if (new URL(request.url).origin !== self.location.origin) {
     return;
   }
 
-  // HTMLは常に最新取得
-  if (request.headers.get('accept')?.includes('text/html')) {
-    event.respondWith(fetch(request));
-    return;
-  }
-
-  // それ以外はキャッシュ優先
   event.respondWith(
-    caches.match(request).then(cachedResponse => {
-      return cachedResponse || fetch(request).then(response => {
+    fetch(request)
+      .then(response => {
         const responseClone = response.clone();
         caches.open(CACHE_NAME).then(cache => {
           cache.put(request, responseClone);
         });
         return response;
-      });
-    })
+      })
+      .catch(() => caches.match(request))
   );
 });
